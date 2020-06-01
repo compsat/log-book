1afee582ff58cfca17f160c1e29942b19887ceec
Charlize Del Rosario <charlizedelrosario@gmail.com>
2020-06-01 21:14:05 +0800

    Added custom theme

---
+++ themes/log-book/header.php
@@ -0,0 +1,8 @@
+<div class="blog-masthead">
+	<div class="container">
+		<nav class="blog-nav">
+			<a class="blog-nav-item active" href="#">Home</a>
+			<?php wp_list_pages( '&title_li=' ); ?>
+		</nav>
+	</div>
+</div>
